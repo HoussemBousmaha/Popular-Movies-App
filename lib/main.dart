3cd1934145026f00796d33381cdfbe8aeb780c86
Youcef Houssem Eddine Bousmaha <ky_bousmaha@esi.dz>
2022-03-27 10:02:47 +0300

    Creating the ApiKey and ApiEndPoint and fetching Information from The TMDB database.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+const String apiKey = '7832b95dc9b342ffe3f9cf9b6d8f091b';
+final apiEndPoint = Uri.parse('http://api.themoviedb.org/3/discover/movie?api_key=$apiKey&sort_by=popularity.desc');
 
 void main() async {
+  final apiResponse = await http.get(apiEndPoint);
+  // print(apiResponse.body);
   runApp(const MovieApp());
 }
 

b4f32aeaf0ee27ababbb549b09a0463120633bee
Youcef Houssem Eddine Bousmaha <ky_bousmaha@esi.dz>
2022-03-27 11:10:06 +0300

    Creating the Movies Provider Class to handle all Movie API related tasks in it

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-const String apiKey = '7832b95dc9b342ffe3f9cf9b6d8f091b';
-final apiEndPoint = Uri.parse('http://api.themoviedb.org/3/discover/movie?api_key=$apiKey&sort_by=popularity.desc');
+void main() async => runApp(const MovieApp());
 
-void main() async {
-  final apiResponse = await http.get(apiEndPoint);
-  // print(apiResponse.body);
-  runApp(const MovieApp());
+class MovieApp extends StatefulWidget {
+  const MovieApp({Key? key}) : super(key: key);
+
+  @override
+  State<MovieApp> createState() => _MovieAppState();
 }
 
-class MovieApp extends StatelessWidget {
-  const MovieApp({Key? key}) : super(key: key);
+class _MovieAppState extends State<MovieApp> {
+  // Variable to store movie data
+  var movies;
+
+  // Method to fetch movies
+  void fetchMovies() async {
+    //Store JSON response in `data` variable
+    var data = await MoviesProvider.getJson();
+
+    //The `setState` method triggers to rebuild intereface
+    setState(() => movies = data['results']);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,5 +36,23 @@ class MovieApp extends StatelessWidget {
       // Main Home Page.
       home: Scaffold(),
     );
+  }
+}
+
+class MoviesProvider {
+  static const String imagePathPrefix = 'https://image.tmdb.org/t/p/w500/';
+
+  static Future<Map> getJson() async {
+    //API Key: To be replaced with your key
+    const apiKey = '7832b95dc9b342ffe3f9cf9b6d8f091b';
+
+    //URL endpoint to fetch popular movies
+    final apiEndPoint = Uri.parse('http://api.themoviedb.org/3/discover/movie?api_key=$apiKey&sort_by=popularity.desc');
+
+    //Making Http request
+    final apiResponse = await http.get(apiEndPoint);
+
+    //Parsing data using `dart:convert` library
+    return json.decode(apiResponse.body);
   }
 }
